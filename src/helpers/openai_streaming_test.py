afdb45fcb84113cc81481788cf76e41731f00a0a
Roger Ngo <urbanspr1nter@gmail.com>
2026-05-21 11:08:39 -0700

    completing the streaming example

--- src/helpers/openai_streaming_test.py
+++ src/helpers/openai_streaming_test.py
@@ -1,12 +1,15 @@
+import json
 import asyncio
 import openai
 from openai.types.responses import (
     ResponseStreamEvent,
     ResponseReasoningItem,
     ResponseOutputMessage,
+    ResponseFunctionToolCall,
 )
 from tools.registry import TOOL_SET
 from typing import Literal
+from tools.call_tool import call_tool
 
 
 def step(
@@ -22,17 +25,40 @@ def step(
         output_item_type = event.item.type
 
         if output_item_type == "reasoning":
+            if machine_state == "tool_call":
+                token += "</tool_call>"
+
             next_state = "reasoning"
             token += "<think>"
+        elif output_item_type == "function_call":
+            if machine_state == "reasoning":
+                token += "</think>"
+            elif machine_state == "tool_call":
+                token += "</tool_call>"
 
+            next_state = "tool_call"
+            token += "<tool_call>"
+
+            item: ResponseFunctionToolCall = event.item
+            token += f"type={item.type};call_id={item.call_id};name={item.name}"
+
+            if item.status != "in_progress":
+                token += f";arguments={item.arguments}"
+                token += "</tool_call>"
+            else:
+                token += ";argument="
         elif output_item_type == "message":
             next_state = "message"
 
             if machine_state == "reasoning":
                 token += "</think>"
+            elif machine_state == "tool_call":
+                token += "</tool_call>"
         else:
             pass
     elif type == "response.reasoning_text.delta":
+        token += event.delta
+    elif type == "response.function_call_arguments.delta":
         token += event.delta
     elif type == "response.output_text.delta":
         token += event.delta
@@ -50,6 +76,8 @@ async def stream_message(client: openai.AsyncClient, context: list):
             model="gemma-4-e2b-it", tools=TOOL_SET, stream=True, input=context
         )
 
+        tool_call_queue = []
+
         async for event in stream_response:
             # print(event.to_json())
 
@@ -63,6 +91,17 @@ async def stream_message(client: openai.AsyncClient, context: list):
                     if completed.type == "reasoning":
                         completed_reasoning: ResponseReasoningItem = completed
                         pass
+                    elif completed.type == "function_call":
+                        completed_tool_call: ResponseFunctionToolCall = completed
+                        context.append(
+                            {
+                                "type": completed_tool_call.type,
+                                "call_id": completed_tool_call.call_id,
+                                "name": completed_tool_call.name,
+                                "arguments": completed_tool_call.arguments,
+                            }
+                        )
+                        tool_call_queue.append(context[-1])
                     elif completed.type == "message":
                         completed_message: ResponseOutputMessage = completed
                         context.append(
@@ -88,15 +127,38 @@ async def stream_message(client: openai.AsyncClient, context: list):
                     token = token.replace("<think>", "\n<think>\n", 1)
                 elif "</think>" in token:
                     token = token.replace("</think>", "\n</think>\n", 1)
+                elif "</tool_call>" in token:
+                    token = token.replace("</tool_call>", "</tool_call>\n", 1)
 
                 print(token, end="", flush=True)
 
-                if current_state == "reasoning" and next_state == "message":
+                if (
+                    current_state == "reasoning" or current_state == "tool_call"
+                ) and next_state == "message":
                     print()
 
                 current_state = next_state
 
-        if current_state == "message":
+        if current_state == "tool_call":
+            # iterate through the tool calls
+            for tool_call in tool_call_queue:
+                name = tool_call["name"]
+
+                if not tool_call["arguments"]:
+                    tool_call["arguments"] = "{}"
+
+                arguments = json.loads(tool_call["arguments"])
+
+                result = call_tool(name, arguments)
+
+                context.append(
+                    {
+                        "type": "function_call_output",
+                        "call_id": tool_call["call_id"],
+                        "output": json.dumps(result),
+                    }
+                )
+        elif current_state == "message":
             print()
             break
 
@@ -108,7 +170,12 @@ async def main():
         {
             "type": "message",
             "role": "user",
-            "content": [{"type": "input_text", "text": "hi there"}],
+            "content": [
+                {
+                    "type": "input_text",
+                    "text": "hi what is the square root of 289? Then tell me what is this: 7^5.",
+                }
+            ],
         }
     ]
 
